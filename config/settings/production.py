9f09e9e435155737135369d2bf6bccc8826b4ec2
OmJangid206 <omprakashjangid206@gmail.com>
2025-03-16 18:06:52 +0530

    Change TLS to SSL for email delivery

--- config/settings/production.py
+++ config/settings/production.py
@@ -29,8 +29,10 @@ DATABASES['default'] = {
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = False
+# EMAIL_PORT = 587
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
