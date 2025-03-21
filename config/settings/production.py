a6a024cd1ec5c1f602d2bc5d48d7021236e60e2b
OmJangid206 <omprakashjangid206@gmail.com>
2025-03-21 21:42:24 +0530

    Update CSS and images for admin panel

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-DEBUG = os.environ["DEBUG"] 
+DEBUG = os.getenv("DEBUG", False)
 ALLOWED_HOSTS = [".vercel.app"]
 
 # database settings (PostgreSQL)
